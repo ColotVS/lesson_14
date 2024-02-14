8b983d518a8bae73e761aba1af3b99078a55562f
ColotVS <kolotilov98@mail.ru>
2024-02-14 19:34:21 +0700

    Методы класса Collections, порядок элементов Урок 14, задача 14

---
+++ lesson_14_14.java
@@ -0,0 +1,45 @@
+public class lesson_14_14 {
+    //Методы класса Collections
+
+    //Порядок элементов
+
+    //Метод Collections.reverse(List<T> list)
+    //Метод reverse() меняет порядок переданных в него элементов списка на обратный.
+
+    //  ArrayList<Integer> list = new ArrayList<Integer>();
+    //  Collections.addAll(list, 1, 2, 3, 4, 5);
+    //  Collections.reverse(list);
+    //  for (int i: list)
+    //      System.out.println(i);
+
+
+    //Метод Collections.sort(List<T> list)
+    //Метод sort() сортирует переданный в него список по возрастанию.
+
+    //  ArrayList<Integer> list = new ArrayList<Integer>();
+    //  Collections.addAll(list, 11, 2, 23, 4, 15);
+    //  Collections.sort(list);
+    //  for (int i: list)
+    //      System.out.println(i);
+
+
+    //Метод Collections.rotate(List<T> list, int distance)
+    //Метод rotate() циклическим образом сдвигает переданные в него элементы на distance позиций вперед.
+
+    //  ArrayList<Integer> list = new ArrayList<Integer>();
+    //  Collections.addAll(list, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    //  Collections.rotate(list, 3); // сдвинуть на 3 позиции
+    //  for (int i: list)
+    //      System.out.println(i);
+
+
+    //Метод Collections.shuffle(List<T> list)
+    //Метод shuffle() случайным образом перемешивает все элементы переданного списка. Результат каждый раз разный.
+
+    //  ArrayList<Integer> list = new ArrayList<Integer>();
+    //  Collections.addAll(list, 1, 2, 3, 4, 5);
+    //  Collections.shuffle(list); // перемешиваем
+    //  for (int i: list)
+    //      System.out.println(i);
+
+}
